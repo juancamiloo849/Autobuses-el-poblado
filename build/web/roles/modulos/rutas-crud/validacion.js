6a5ea87f02aa0e2b2013cd22a26afc4720722720
adsi2 <adsi2@adsi2-H81M-H>
2018-03-09 10:02:48 -0500

    modificar index

--- build/web/roles/modulos/rutas-crud/validacion.js
+++
@@ -1,9 +0,0 @@
-var placas = document.getElementById("placa");
-
-email.addEventListener("keyup", function (event) {
-  if (placas.validity.typeMismatch) {
-    placas.setCustomValidity("¡Yo esperaba una PLACA de vehículo, cariño!");
-  } else {
-    placas.setCustomValidity("");
-  }
-});
